3aab3b88daa1f417a898bb7dc494384fa2419deb
Harry Bond <86840934+harry-the-fish@users.noreply.github.com>
2024-09-12 16:24:22 +0100

    Buttons turn white when selected navigation page and updated routes for each page

--- controllers/general-controller.rb
+++ controllers/general-controller.rb
@@ -2,11 +2,21 @@ get '/' do # Redirects straight to /home
     redirect '/home'
 end
 
+get '/home' do # Shows landing page
+    @teams = Team.all.sort_by { |team| -team[:points] }
+    erb :landing_page
+end
+
 get '/our-team' do # Shows landing page
     @players = Player.all
     erb :our_team
 end
 
-get '/home' do # Shows landing page
-    erb :landing_page
+get '/player-statistics' do # Shows landing page
+    @teams = Team.all.sort_by { |team| -team[:points] }
+    erb :player_statistics
+end
+
+get '/as-it-stands-awards' do # Shows landing page
+    erb :as_it_stands_awards
 end
